4fcdc4ec90c49824dacc0d41526387052eb8c90d
Dylan Gattey <dylan.gattey@gmail.com>
2020-10-28 23:17:59 -0700

    chore: draws a blue screen, generator file created

--- Land macOS/GameViewController.swift
+++ Land macOS/GameViewController.swift
@@ -11,34 +11,27 @@ import MetalKit
 // Our macOS specific view controller
 class GameViewController: NSViewController {
 
-    var renderer: Renderer!
-    var mtkView: MTKView!
+    var renderer: Renderer?
+    var mainView: MTKView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        guard let mtkView = self.view as? MTKView else {
-            print("View attached to GameViewController is not an MTKView")
+        guard let mainView = self.view as? MTKView,
+              let defaultDevice = MTLCreateSystemDefaultDevice() else {
+            print("Metal isn't set up correctly")
             return
         }
 
-        // Select the device to render with.  We choose the default device
-        guard let defaultDevice = MTLCreateSystemDefaultDevice() else {
-            print("Metal is not supported on this device")
-            return
-        }
-
-        mtkView.device = defaultDevice
-
-        guard let newRenderer = Renderer(metalKitView: mtkView) else {
+        guard let newRenderer = Renderer(mainView: mainView, device: defaultDevice) else {
             print("Renderer cannot be initialized")
             return
         }
-
+        // Set the size once to kick off initial drawing process
+        newRenderer.mtkView(mainView, drawableSizeWillChange: mainView.drawableSize)
+        
+        // Make sure the view's delegate is our new renderer, and save it
+        mainView.delegate = newRenderer
         renderer = newRenderer
-
-        renderer.mtkView(mtkView, drawableSizeWillChange: mtkView.drawableSize)
-
-        mtkView.delegate = renderer
     }
 }
