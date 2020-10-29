146ac986b0c6026c6cfeb33a1d87e775d78fc20c
Dylan Gattey <dylan.gattey@gmail.com>
2020-10-29 15:06:25 -0700

    chore: swiftier code in Renderer.swift

--- rendering/Renderer.swift
+++ rendering/Renderer.swift
@@ -11,6 +11,15 @@ import Metal
 import MetalKit
 import simd
 
+// Constants for the renderer
+private enum Constant {
+    static let vertexFunction = "vertexShader"
+    static let fragmentFunction = "fragmentShader"
+    static let backgroundColor = MTLClearColorMake(0.0, 0.5, 1.0, 1.0)
+}
+
+// MARK: - Renderer Class
+
 class Renderer: NSObject, MTKViewDelegate {
     
     private let view: MTKView
@@ -19,6 +28,9 @@ class Renderer: NSObject, MTKViewDelegate {
     private let renderPipelineState: MTLRenderPipelineState
     private var currentViewport: MTLViewport
     
+    // MARK: - initialization
+    
+    // If the command queue or pipeline state fails to get created, this will fail
     init?(view: MTKView, device: MTLDevice) {
         // Create related objects
         guard let commandQueue = device.makeCommandQueue(),
@@ -37,39 +49,35 @@ class Renderer: NSObject, MTKViewDelegate {
         super.init()
     }
     
-    // Builds a render pipeline state object using the current device and our shader
-    static func buildPipelineState(view: MTKView, device: MTLDevice) -> MTLRenderPipelineState? {
+    // Builds a render pipeline state object using the current device and our default shaders
+    private static func buildPipelineState(view: MTKView, device: MTLDevice) -> MTLRenderPipelineState? {
         let library = device.makeDefaultLibrary()
-        let vertexFunction = library?.makeFunction(name: "vertexShader")
-        let fragmentFunction = library?.makeFunction(name: "fragmentShader")
+        let vertexFunction = library?.makeFunction(name: Constant.vertexFunction)
+        let fragmentFunction = library?.makeFunction(name: Constant.fragmentFunction)
 
         let descriptor = MTLRenderPipelineDescriptor()
-        descriptor.label = "BasicPipeline"
         descriptor.vertexFunction = vertexFunction
         descriptor.fragmentFunction = fragmentFunction
-        
-        // Copy pixel format since this is a simple pipeline
         descriptor.colorAttachments[0].pixelFormat = view.colorPixelFormat
         
-        // Make it!
         var stateObject: MTLRenderPipelineState?
         do {
             try stateObject = device.makeRenderPipelineState(descriptor: descriptor)
         } catch let error {
-            print(error)
+            print("Couldn't create render pipeline state object: \(error)")
+            return nil
         }
         return stateObject
     }
     
     // Configures the view itself once with everything it needs to start to render
-    static func configure(view: MTKView, device: MTLDevice) {
+    private static func configure(view: MTKView, device: MTLDevice) {
         view.device = device
-        view.clearColor = MTLClearColorMake(0.0, 0.5, 1.0, 1.0)
+        view.clearColor = Constant.backgroundColor
         view.enableSetNeedsDisplay = true
     }
     
-    // No-op, called on every tick to update data
-    private func updateGameState() {}
+    // MARK: - MTKViewDelegate functions
     
     // Set the new viewport size for next draw pass
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
@@ -78,22 +86,34 @@ class Renderer: NSObject, MTKViewDelegate {
     
     // Updates state and draws something to the screen
     func draw(in view: MTKView) {
-        updateGameState()
-
         guard let commandBuffer = commandQueue.makeCommandBuffer(),
               let descriptor = view.currentRenderPassDescriptor,
               let encoder = commandBuffer.makeRenderCommandEncoder(descriptor: descriptor) else {
             print("Error in drawing stage")
             return
         }
-        // Start drawing
+        
+        // Configure the encoder & draw to it
         encoder.setViewport(currentViewport)
         encoder.setRenderPipelineState(renderPipelineState)
+        draw(to: encoder)
+        encoder.endEncoding()
         
+        // Draw to the screen itself and commit what we've enqueued
+        if let drawable = view.currentDrawable {
+            commandBuffer.present(drawable)
+        }
+        commandBuffer.commit()
+    }
+    
+    // MARK: - drawing functions
+    
+    // Queues the sample triangle to the encoder
+    private func draw(to encoder: MTLRenderCommandEncoder) {
         let positions: [Float] = [
-             0.0,  250,
+            0.0,  250,
             -250, -250,
-             250, -250
+            250, -250
         ]
         let triBytes = mainDevice.makeBuffer(bytes: positions, length: positions.count * 32, options: .storageModeShared)
         let colors: [Float] = [
@@ -111,23 +131,14 @@ class Renderer: NSObject, MTKViewDelegate {
         encoder.setVertexBuffer(triBytes, offset: 0, index: VertexAttribute.positions.rawValue)
         encoder.setVertexBuffer(colorBytes, offset: 0, index: VertexAttribute.colors.rawValue)
         encoder.setVertexBuffer(viewportBytes, offset: 0, index: VertexAttribute.viewportSize.rawValue)
-       
+        
         encoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3)
-        
-        // Done with drawing instructions
-        encoder.endEncoding()
-        
-        // Draw to the screen itself
-        if let drawable = view.currentDrawable {
-            commandBuffer.present(drawable)
-        }
-        
-        // We're done with commands for this frame, so commit what we've queued up
-        commandBuffer.commit()
     }
     
     
 }
+
+// MARK: - Convenience functions
 
 // Convenience function for creating a Viewport from a regular CGSize
 func viewport(from size: CGSize) -> MTLViewport {
