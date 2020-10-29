c35e7b4cdcf13ad9a4cd95e21c16faecd7bc83f7
Dylan Gattey <dylan.gattey@gmail.com>
2020-10-28 22:06:01 -0700

    chore: initial commit

---
+++ Land macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Land macOS
+//
+//  Created by Dylan Gattey on 10/28/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
